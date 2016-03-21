923b2b32cc9f9b6a9804fa5d175e713bbe206ea1
EnioVedovatto <tio.enio@hotmail.com>
2016-03-20 21:40:48 -0300

    adicionado as models Role e User.php

--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -7,5 +7,37 @@
 			# code...
 		} */
 
-		
+		public function cadastrarAction()
+		{
+			$this->view->setFile('index');
+
+			$this->request->setCustomFilters
+				(array
+					(
+						'email' => FILTER_VALIDATE_EMAIL
+					));
+
+			$post = $this->request->post();
+
+			if (!empty($post))
+			{
+				$cadastrarUsuario = User::cadastrar($post);
+
+				if ($cadastrarUsuario->status === false)
+				{
+					$this->load('Helpers\Alert', array
+					(
+						'danger',
+						'Ops! Não foi possível efetuar seu cadastro. <br> Verifique os erros abaixo:',
+						$cadastrarUsuario->errors
+					));
+				}
+
+				else
+				{
+					$this->auth->login($cadastrarUsuario->user->id, $cadastrarUsuario->user->username);
+				}
+			}
+		}
+
 	}
